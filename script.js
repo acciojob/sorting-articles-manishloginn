97170103e6b64e0b7a1a84d1e8666b39b17a76be
manishloginn <132683386+manishloginn@users.noreply.github.com>
2024-02-12 10:58:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
 //your JS code here. If required.
+function removeArticles(name) {
+  const articles = ["a", "an", "the"];
+  for (const article of articles) {
+    if (name.toLowerCase().startsWith(article + " ")) {
+      return name.slice(article.length + 1).trim();
+    }
+  }
+  return name;
+}
+ 
+function updateWebPage(bands) {
+  const ul = document.getElementById("bands");
+  bands.forEach((element) => {
+    const li = document.createElement("li");
+    li.innerText = element;
+    ul.appendChild(li);
+  });
+}
+ 
+const bands = [
+  "The Plot in You",
+  "The Devil Wears Prada",
+  "Pierce the Veil",
+  "Norma Jean",
+  "The Bled",
+  "Say Anything",
+  "The Midway State",
+  "We Came as Romans",
+  "Counterparts",
+  "Oh, Sleeper",
+  "A Skylit Drive",
+  "Anywhere But Here",
+  "An Old Dog",
+];
+ 
+bands.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+ 
+updateWebPage(bands);
